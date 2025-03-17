580c8455c95780983f6ceef0cd6ecabe0eb8e09a
aniran <aniran530@gmail.com>
2025-03-17 19:52:35 +0530

    project configuration for Squash the Creeps

--- .godot/mono/temp/obj/Debug/Squash the Creeps (3D).AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Squash the Creeps (3D).AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Squash the Creeps (3D)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40fc812039a9495bc7987ed5100d8ddd035a8ac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Squash the Creeps (3D)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Squash the Creeps (3D)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
